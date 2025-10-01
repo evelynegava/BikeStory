ce4ffe8478dbf97175418c3ac3f67ef4ee314993
evelynegava <61299183+evelynegava@users.noreply.github.com>
2025-10-01 11:04:26 -0300

    Listar os Produtos que não tenham sido comprados

--- consultas.sql
+++ consultas.sql
@@ -6,3 +6,11 @@ Where not exists (
     From orders o
     Where c.customer_id = o.customer_id
 )
+
+--Listar os Produtos que não tenham sido comprados
+Select * 
+  From products p 
+  Where not exists 
+  (Select 1 
+   From order_itens oi 
+   Where p.product_id = oi.product_id)
